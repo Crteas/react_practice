7d1620407e5bb6406738ed21b36ec1b08ab7d49c
Pru <lms314044s2@gmail.com>
2022-08-26 17:31:01 +0900

    React Coin Tracker
    just func

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currntArray) => [toDo, ...currntArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [price, setPrice] = useState(1);
+  const [money, setMoney] = useState(1);
+  const coinChange = (event) => {
+    const { target } = event;
+    const coinPrice = target[target.selectedIndex].value;
+    setPrice(coinPrice);
   };
-
+  const moneyChange = (event) => {
+    setMoney(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  console.log(money, price);
   return (
     <div>
-      <h1>My To Dos : {toDos.length}</h1>
-      <form>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type={"text"}
-          placeholder="hello"
-        />
-        <button onClick={onSubmit}>ADD TO DO</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coin ({coins.length})</h1>
+      {loading ? <strong>Loading.....</strong> : null}
+      <select onChange={coinChange}>
+        <option>select Coin</option>
+        {coins.map((coin, index) => (
+          <option key={index} value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>
+      <input type={"number"} value={money} onChange={moneyChange} />
+      <strong>{(money / price).toFixed(2)}</strong>
     </div>
   );
 }
